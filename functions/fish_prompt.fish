35093ac32aa9acc693d7a12c55071859b014cb10
John Mettraux <jmettraux@gmail.com>
2019-04-04 21:01:30 +0900

    Fix "maru"

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -78,7 +78,7 @@ set -g __fish_git_prompt_char_upstream_prefix ""
 set -g __fish_git_prompt_char_stagedstate "."
 set -g __fish_git_prompt_char_dirtystate "+"
 set -g __fish_git_prompt_char_conflictedstate "×"
-set -g __fish_git_prompt_char_cleanstate "○"
+set -g __fish_git_prompt_char_cleanstate "o"
 set -g __fish_git_prompt_char_untrackedfiles "…"
 set -g __fish_git_prompt_char_stateseparator "|"
 
