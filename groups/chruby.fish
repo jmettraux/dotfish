17dcc2aea922035adb18a2a6232136ed51372732
John Mettraux <jmettraux@gmail.com>
2019-05-13 14:22:42 +0900

    Refine chruby.fish, especially its $PATH

--- groups/chruby.fish
+++ groups/chruby.fish
@@ -4,7 +4,12 @@ function chruby
   set -q FISH_NO_CRUBY; if test $status = 0; return; end
   if test ! -e .ruby-version; return; end
 
-  set -l rubyver (ls ~/.rubies/ | grep (cat .ruby-version))
+  # clear, change ruby
+
+  set -l rubydir (realpath ~/.rubies)
+  set -l gemdir (realpath ~/.gem)
+
+  set -l rubyver (ls $rubydir | grep (cat .ruby-version))
   set -l rubyev (string split "-" -- $rubyver)
   #echo $rubyver
   #echo $rubyev
@@ -14,8 +19,8 @@ function chruby
   #echo "\$RUBY_ENGINE: $RUBY_ENGINE"
   #echo "\$RUBY_VERSION: $RUBY_VERSION"
 
-  set -gx RUBY_ROOT ~/.rubies/$rubyver
-  set -gx GEM_HOME ~/.gem/ruby/$RUBY_VERSION
+  set -gx RUBY_ROOT $rubydir/$rubyver
+  set -gx GEM_HOME $gemdir/$RUBY_ENGINE/$RUBY_VERSION
   set -gx GEM_ROOT $RUBY_ROOT/lib/ruby/gems/(ls $RUBY_ROOT/lib/ruby/gems)[1]
   set -gx GEM_PATH $GEM_HOME:$GEM_ROOT
   #echo "\$RUBY_ROOT: $RUBY_ROOT"
@@ -23,7 +28,19 @@ function chruby
   #echo "\$GEM_ROOT: $GEM_ROOT"
   #echo "\$GEM_PATH: $GEM_PATH"
 
-  set -gx PATH $GEM_HOME/bin:$GEM_ROOT/bin:$RUBY_ROOT/bin:$PATH
+  set -l rl (string length $rubydir)
+  set -l gl (string length $gemdir)
+  set -l path
+    #
+  for pa in $PATH
+    set pa (realpath $pa)
+    if test (string sub -l $rl $pa) = $rubydir; continue; end
+    if test (string sub -l $gl $pa) = $gemdir; continue; end
+    if contains $pa $path; continue; end
+    set path $path $pa
+  end
+    #
+  set -gx PATH $GEM_HOME/bin $GEM_ROOT/bin $RUBY_ROOT/bin $path
   #echo "\$PATH: $PATH"
 
   echo "ruby set to $RUBY_ROOT"
