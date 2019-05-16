b0db726be5a5f8f6a645a883ddebbbea9c589fcb
John Mettraux <jmettraux@gmail.com>
2019-05-17 08:58:00 +0900

    Go with fortune -a

--- functions/fish_greeting.fish
+++ functions/fish_greeting.fish
@@ -1,7 +1,7 @@
 
 function fish_greeting
   #set -l c_lightgrey (set_color A9A9A9)
-  echo (set_color A9A9A9); if type -q fortune; fortune; end
+  echo (set_color A9A9A9); if type -q fortune; fortune -a; end
   echo (set_color 292929); uname -a
   echo (set_color normal)
 end
