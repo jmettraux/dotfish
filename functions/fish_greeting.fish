dc49d0d149443b4cf441c15c84b3c3e5b8bd0b3b
John Mettraux <jmettraux@gmail.com>
2020-01-18 15:49:56 +0900

    Stop fortune for now

--- functions/fish_greeting.fish
+++ functions/fish_greeting.fish
@@ -3,12 +3,12 @@ function fish_greeting
 
   #set -l c_lightgrey (set_color A9A9A9)
 
-  echo (set_color A9A9A9)
-  if type -q fortune
-    fortune -a
-  else if type -q /usr/games/fortune
-    /usr/games/fortune -a
-  end
+  #echo (set_color A9A9A9)
+  #if type -q fortune
+  #  fortune -a
+  #else if type -q /usr/games/fortune
+  #  /usr/games/fortune -a
+  #end
 
   echo (set_color 292929)
   uname -a
