485144e47d068f245e8899f931c364708d9df126
John Mettraux <jmettraux@gmail.com>
2020-01-18 06:42:15 +0000

    Refine ssh-agent checks

--- groups/ssh.fish
+++ groups/ssh.fish
@@ -22,12 +22,10 @@ function test_identities
   end
 end
 
-if [ -f $HOME/.ssh/no.agent.fish ]
-  # a cheap way out on servers
-  # do nothing
-else if [ -n "$SSH_AGENT_PID" ]
+if [ -n "$SSH_AUTH_SOCK" ]
   #ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
-  ps aux | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
+  #ps aux | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
+  ps aux | grep "ssh-agent -c" > /dev/null
   if [ $status -eq 0 ]
     test_identities
   end
