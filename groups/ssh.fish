f82cfe087fa8020722bb9f284db2b37e948c37a5
John Mettraux <jmettraux@gmail.com>
2020-01-22 14:40:51 +0900

    Mute ssh-agent tinkering

--- groups/ssh.fish
+++ groups/ssh.fish
@@ -4,9 +4,9 @@
 setenv SSH_ENV $HOME/.ssh/agent.fish.env
 
 function start_agent
-  echo "(ssh.fish) initializing new SSH agent..."
+  #echo "(ssh.fish) initializing new SSH agent..."
   ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
-  echo "(ssh.fish) succeeded."
+  #echo "(ssh.fish) succeeded."
   chmod 600 $SSH_ENV
   . $SSH_ENV > /dev/null
   ssh-add
@@ -22,15 +22,15 @@ function test_identities
   end
 end
 
-echo (set_color 292929)
+#echo (set_color 292929)
 
 if test -n "$SSH_CONNECTION"
 
-  echo "(ssh.fish) connecting in ($SSH_CONNECTION), trusting agent forwarding."
+  #echo "(ssh.fish) connecting in ($SSH_CONNECTION), trusting agent forwarding."
 
 else if test -n "$SSH_AGENT_PID"
 
-  echo "(ssh.fish) agent already on and known ($SSH_AGENT_PID)..."
+  #echo "(ssh.fish) agent already on and known ($SSH_AGENT_PID)..."
 
   ps aux | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
   if [ $status -eq 0 ]
@@ -39,7 +39,7 @@ else if test -n "$SSH_AGENT_PID"
 
 else
 
-  echo "(ssh.fish) lock to already existing agent ($SSH_ENV)..."
+  #echo "(ssh.fish) lock to already existing agent ($SSH_ENV)..."
 
   if test -f $SSH_ENV
     . $SSH_ENV > /dev/null
@@ -48,12 +48,12 @@ else
   if test $status -eq 0
     test_identities
   else
-    echo "(ssh.fish) ouch, have to start an agent..."
+    #echo "(ssh.fish) ouch, have to start an agent..."
     start_agent
   end
 end
 
 # use `set -e SSH_AGENT_PID` to unset
 
-echo -n (set_color normal)
+#echo -n (set_color normal)
 
