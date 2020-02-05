bd2c3fc6d504ec3a9c358771e8072729c76e0e9d
John Mettraux <jmettraux@gmail.com>
2020-02-05 21:08:45 +0000

    Only bind keys in interactive sessions

--- config.fish
+++ config.fish
@@ -1,10 +1,19 @@
 
-fish_vi_key_bindings
-set -gx EDITOR vim
+if status is-interactive
+  #
+  # to avoid output like this when using rsync, wrap those bind calls
+  # ---8<---
+  # bind: No key with name 'right' found
+  # bind: No key with name 'left' found
+  # --->8---
 
-#bind -M insert -k right forward-char # right-arrow
-bind -M insert ] forward-char
-#bind \cd delete-char
+  fish_vi_key_bindings
+  set -gx EDITOR vim
+
+  #bind -M insert -k right forward-char # right-arrow
+  bind -M insert ] forward-char
+  #bind \cd delete-char
+end
 
 set -x PATH ~/bin $PATH
 
