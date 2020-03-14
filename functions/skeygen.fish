29a6062cd657489cff7f3c1a4364a58d3678737d
John Mettraux <jmettraux@gmail.com>
2020-03-14 19:30:43 +0900

    Refine skeygen

--- functions/skeygen.fish
+++ functions/skeygen.fish
@@ -3,6 +3,8 @@ function skeygen
 
   set fname $argv[1]
 
+  if test "$fname" = ""; echo "USAGE: skeygen {fname}"; return 1; end
+
   ssh-keygen -t ed25519 -a 100 -f $fname -C "jmettraux+$fname@gmail.com"
 end
 
