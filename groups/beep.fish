dd5a976284bf824097201dab1b22cdf887752af3
John Mettraux <jmettraux@gmail.com>
2021-09-01 16:35:21 +0900

    Let beep accept a number argument

--- groups/beep.fish
+++ groups/beep.fish
@@ -1,6 +1,12 @@
 
 function beep
 
-  aucat -i "$HOME/.fish/groups/beep.wav"
+  set count 1
+  if test -n "$argv[1]"; set count (math 0 + $argv[1]); end
+
+  for x in (seq $count)
+    sleep 0.100
+    aucat -i "$HOME/.fish/groups/beep.wav"
+  end
 end
 
