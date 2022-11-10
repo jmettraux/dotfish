d64ae148a06e9096a55d478fe00031026d46bcee
John Mettraux <jmettraux@gmail.com>
2022-11-10 16:38:28 +0900

    Use jdk-17 over jdk-11 if available

--- groups/java.fish
+++ groups/java.fish
@@ -1,7 +1,9 @@
 
 # java
 
-if test -d /usr/local/jdk-11/bin
+if test -d /usr/local/jdk-17/bin
+  set -x PATH /usr/local/jdk-17/bin $PATH
+else if test -d /usr/local/jdk-11/bin
   set -x PATH /usr/local/jdk-11/bin $PATH
 end
 
