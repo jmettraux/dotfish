e4ebec233ecde713d168a24847c7bdde4e26f438
John Mettraux <jmettraux@gmail.com>
2024-04-19 08:26:52 +0900

    Introduce fe0 alias to feh

--- groups/feh.fish
+++ groups/feh.fish
@@ -4,6 +4,9 @@
 function fe
   feh -d -F --info "echo '%F %s %S %hx%w %z'" $argv
 end
+function fe0
+  feh -d --info "echo '%F %s %S %hx%w %z'" $argv
+end
 
 function qfe
   qrencode $argv -o - | feh -d -F -
