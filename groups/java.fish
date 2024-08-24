fb633c4fb7043f6a5f2ea9449b47e6417e85a65f
John Mettraux <jmettraux@gmail.com>
2024-08-25 08:51:03 +0900

    Refine groups/java.fish adding jdk to path

--- groups/java.fish
+++ groups/java.fish
@@ -1,9 +1,18 @@
 
-# java
+#
+# groups/java.fish
 
-if test -d /usr/local/jdk-17/bin
-  set -x PATH /usr/local/jdk-17/bin $PATH
-else if test -d /usr/local/jdk-11/bin
-  set -x PATH /usr/local/jdk-11/bin $PATH
+#if test -d /usr/local/jdk-17/bin
+#  set -x PATH /usr/local/jdk-17/bin $PATH
+#else if test -d /usr/local/jdk-11/bin
+#  set -x PATH /usr/local/jdk-11/bin $PATH
+#end
+
+for dir in (ls -d /usr/local/jdk-* | sort -rV)
+
+  if test -d $dir
+
+    set -x PATH $dir/bin $PATH
+    break
+  end
 end
-
